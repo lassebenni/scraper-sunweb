40bcffc48a86ec2589607adcf8f191fd68ffc454
Lasse Benninga <devops.pipeline@example.com>
2022-10-05 10:43:16 +0200

    fix

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0',
     'Accept': 'application/json',
     'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
