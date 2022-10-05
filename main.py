65ddb612d1135606050f8b1ac449d90402d132fb
Lasse Benninga <devops.pipeline@example.com>
2022-10-05 22:48:08 +0200

    Flatten the results

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+from time import sleep
 from typing import Dict, List
 import requests
 import json
+import math
+import collections
 
+def flatten(dictionary, parent_key=False, separator='.'):
+    """
+    Turn a nested dictionary into a flattened dictionary
+    :param dictionary: The dictionary to flatten
+    :param parent_key: The string to prepend to dictionary's keys
+    :param separator: The string used to separate flattened keys
+    :return: A flattened dictionary
+    """
+
+    items = []
+    for key, value in dictionary.items():
+        new_key = str(parent_key) + separator + key if parent_key else key
+        if isinstance(value, collections.MutableMapping):
+            items.extend(flatten(value, new_key, separator).items())
+        elif isinstance(value, list):
+            for k, v in enumerate(value):
+                items.extend(flatten({str(k): v}, new_key).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
 
 
 def scrape_sunweb():
     res = []
 
     offset = 0
-    
+    limit = 50
 
-    url = f"https://www.sunweb.nl/api/sitecore/SearchApi/GetSearchResponse?Duration%5B0%5D=8-10&sort=Price&contextitemid=38b1ea16-284c-40c9-af10-61c5bb8ce520&isFirstUserRequest=True&Allocation=20&isFirstLoad=true&offset=0&limit=50"
+    url = f"https://www.sunweb.nl/api/sitecore/SearchApi/GetSearchResponse?Duration%5B0%5D=8-10&sort=Price&contextitemid=38b1ea16-284c-40c9-af10-61c5bb8ce520&isFirstUserRequest=True&Allocation=20&isFirstLoad=true&offset={offset}&limit={limit}"
 
     payload={}
     headers = {
@@ -22,19 +45,34 @@ def scrape_sunweb():
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
         'TE': 'trailers',
-        # 'Cookie': 'participants={"rooms":[["1989-11-07","1989-11-07"]],"allocation":20}; search-sel={"selectedFilters":[{"filterType":2,"values":["8-10"]}]}; sunwebnl#lang=nl-NL'
     }
 
-    
     response = requests.request("GET", url, headers=headers, data=payload)
-    r = json.loads(response.text)
+    first: List[Dict] = response.json()
+    listings = [flatten(x) for x in first['results']]
+    res.append(listings)
 
+    total_results = first['pagination']['totalResults']
+    total_trips = math.ceil(total_results / limit)
+    current_trip = 1
+
+    while current_trip < total_trips:
+        sleep(1)
+        current_trip += 1
+        offset += limit
+        url = f"https://www.sunweb.nl/api/sitecore/SearchApi/GetSearchResponse?Duration%5B0%5D=8-10&sort=Price&contextitemid=38b1ea16-284c-40c9-af10-61c5bb8ce520&isFirstUserRequest=True&Allocation=20&isFirstLoad=true&offset={offset}&limit={limit}"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        listings: List[Dict] = [flatten(x) for x in first['results']]
+        res.append(listings)
+
+    return [item for sublist in res for item in sublist] # flatten nested list
 
 
 def store(res: List[Dict]):
     with open('results.json', 'w') as f:
         json.dump(res, f)
 
+
 if __name__ == '__main__':
-    scrape_sunweb()
-    store()
+    res = scrape_sunweb()
+    store(res)
