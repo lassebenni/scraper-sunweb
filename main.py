08a600948d4b60193cff3bc693b61c0ece6069f2
Lasse Benninga <devops.pipeline@example.com>
2022-10-05 21:52:42 +0200

    d /Users/lassebenninga/Documents/gdd/scraper-sunweb ; /usr/bin/env /Users/lassebenninga/Documents/gdd/scraper-sunweb/.venv/bin/python /Users/lassebenninga/.vscode/extensions/ms-python.python-2022.14.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 53158 -- /Users/lassebenninga/Documents/gdd/scraper-sunweb/main.py

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
+from typing import Dict, List
 import requests
 import json
 
-url = "https://www.sunweb.nl/api/sitecore/SearchApi/GetSearchResponse?Duration%5B0%5D=8-10&sort=Price&contextitemid=38b1ea16-284c-40c9-af10-61c5bb8ce520&isFirstUserRequest=True&Allocation=20&isFirstLoad=true&offset=0&limit=50"
 
-payload={}
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0',
-    'Accept': 'application/json',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'TE': 'trailers',
-    'Cookie': 'participants={"rooms":[["1989-11-07","1989-11-07"]],"allocation":20}; search-sel={"selectedFilters":[{"filterType":2,"values":["8-10"]}]}; sunwebnl#lang=nl-NL'
-}
 
-response = requests.request("GET", url, headers=headers, data=payload)
-res = json.loads(response.text)
-print(res)
+def scrape_sunweb():
+    res = []
 
-if res:
+    offset = 0
+    
+
+    url = f"https://www.sunweb.nl/api/sitecore/SearchApi/GetSearchResponse?Duration%5B0%5D=8-10&sort=Price&contextitemid=38b1ea16-284c-40c9-af10-61c5bb8ce520&isFirstUserRequest=True&Allocation=20&isFirstLoad=true&offset=0&limit=50"
+
+    payload={}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers',
+        # 'Cookie': 'participants={"rooms":[["1989-11-07","1989-11-07"]],"allocation":20}; search-sel={"selectedFilters":[{"filterType":2,"values":["8-10"]}]}; sunwebnl#lang=nl-NL'
+    }
+
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+    r = json.loads(response.text)
+
+
+
+def store(res: List[Dict]):
     with open('results.json', 'w') as f:
-        json.dump(res['results'], f)
+        json.dump(res, f)
+
+if __name__ == '__main__':
+    scrape_sunweb()
+    store()
